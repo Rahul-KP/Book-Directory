5651666fa0322880ee6ae3addd733f5dae16a081
Rahul-KP <rahulkp01@gmail.com>
2022-02-25 11:46:57 +0530

    Implemented PUT and DELETE

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ app.use(express.json());
 
 app.listen(port,function() {
 	console.log("Listening on port "+ port);
+	console.log(db.JSON());
 });
 
 app.get('/', function(req,res) {
@@ -18,5 +19,29 @@ app.get('/', function(req,res) {
 			res.send(dir);
 		}
 	}
-	// res.send("<h1> pineapple pizza </h1>");
+});
+
+app.post('/',function(req,res) {
+	console.log(req.body);
+	db.set(req.body.isbn,{"author":req.body.author,"title":req.body.title});
+	res.send("Book details added\n");
+});
+
+app.put('/',function(req,res) {
+	res.send("Starting put request");
+	// console.log(req.body);
+	if(db.has(req.body.isbn)) {
+		db.set(req.body.isbn,{"author":req.body.author,"title":req.body.title});
+	}
+	// console.log(db.JSON());
+});
+
+app.delete('/',function(req,res) {
+	if(db.has(req.body.isbn)) {
+		db.delete(req.body.isbn);
+		res.send("Entry deleted");
+	}
+	else {
+		res.send("Entry does not exist");
+	}
 });
